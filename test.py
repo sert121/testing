531e187a5540a5e67a5353ed79b707ff0b034132
sert121 <yashrajmore29@gmail.com>
2021-10-20 23:42:46 +0530

    added files

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import base64
 def get_image_download_link(img,filename,text):
 	img = img.convert('RGB')
 	buffered = BytesIO()
-	img.save(buffered, format="JPEG")
+	img.save(buffered, format="PNG")
 	img_str = base64.b64encode(buffered.getvalue()).decode()
 	href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
 	return href
