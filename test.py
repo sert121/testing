6a6fb5c58f916f439f33e8141b6231b289a0e5bb
sert121 <yashrajmore29@gmail.com>
2021-10-20 23:23:10 +0530

    added files

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import streamlit as st
 from webcam import webcam
 
 captured_image = webcam()
+
 if captured_image is None:
 	st.write("Waiting for capture...")
 else:
 	st.write("Got an image from the webcam:")
+	st.write(type(captured_image))
 	st.image(captured_image)
-	st.download_button(label="Download image",data=captured_image,file_name="default.png")
+	# st.download_button(label="Download image",data=captured_image,file_name="default.png")
 
