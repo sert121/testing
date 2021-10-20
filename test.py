7291360c1b11921f96e16f9822a6af0dd503d40f
sert121 <yashrajmore29@gmail.com>
2021-10-20 23:31:22 +0530

    added files

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ import streamlit as st
 from webcam import webcam
 from io import BytesIO
 
-captured_image = webcam()
-
 def get_image_download_link(img,filename,text):
 	img = img.convert('RGB')
-    buffered = BytesIO()
-    img.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue()).decode()
-    href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
-    return href
+	buffered = BytesIO()
+	img.save(buffered, format="JPEG")
+	img_str = base64.b64encode(buffered.getvalue()).decode()
+	href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+	return href
+
+
+captured_image = webcam()
+
 
 if captured_image is None:
 	st.write("Waiting for capture...")
