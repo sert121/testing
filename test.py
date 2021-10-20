aad35255629298bd8446f66103c39d89e099d224
sert121 <yashrajmore29@gmail.com>
2021-10-20 23:20:21 +0530

    added files

--- test.py
+++ test.py
@@ -3,10 +3,9 @@ from webcam import webcam
 
 captured_image = webcam()
 if captured_image is None:
-    st.write("Waiting for capture...")
+	st.write("Waiting for capture...")
 else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
-
+	st.write("Got an image from the webcam:")
+	st.image(captured_image)
 	st.download_button(label="Download image",data=captured_image,file_name="default.png")
 
